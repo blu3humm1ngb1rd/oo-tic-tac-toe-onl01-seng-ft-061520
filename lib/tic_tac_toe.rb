ba9330d01c2aaba902eb2dd3a4012cb722bbe861
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-27 21:13:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -116,7 +116,16 @@ class TicTacToe
   end 
   
   def winner
-    if 
+    WIN_COMBINATIONS.detect do |winner| 
+      if @board[winner[0]] == "X" &&  @board[winner[1]] == "X" && @board[winner[2]] == "X"
+      return winner 
+      elsif 
+        @board[winner[0]] == "O" &&  @board[winner[1]] == "O" && @board[winner[2]] == "O"
+        return winner 
+      else 
+        nil 
+    end 
+    end 
   end 
   
   def play 
